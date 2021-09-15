6b53afb8f7a38152a5d17ceb802cd8e24b602dff
zaimartinezj <zaidamartinezjarse@gmail.com>
2021-09-15 13:47:14 -0300

    fix css margins

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,10 +11,8 @@ export const Welcome = () => {
                 <div className="sombra"></div>
                 <img className="myphoto" src={ foto } alt="myphoto" width="200" height="200"></img>
             </div>
-            <div className="text-welcome">  
-                <div className="title-writing">
-                    <p className="title welcome-title">Bienvenid@! 👋</p>
-                </div>
+            <div className="text-welcome">            
+                <p className="title welcome-title">Bienvenid@! 👋</p>
                 <p>Mi nombre es Zaida. Soy Desarrolladora Web.</p>
                 <p>Me encanta la tecnología y programar. Siempre en busqueda de nuevos desafíos.</p>
            </div> 
